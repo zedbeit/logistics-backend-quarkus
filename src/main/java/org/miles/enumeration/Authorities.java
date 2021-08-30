246998d2317f78f29a3e0be89dfabaab07c46405
zedbeit <devbeita@gmail.com>
2021-08-30 15:40:19 +0100

    added representations

---
+++ src/main/java/org/miles/enumeration/Authorities.java
@@ -0,0 +1,6 @@
+package org.miles.enumeration;
+
+public enum Authorities {
+    ROLE_ADMIN, ROLE_GENERAL_USER, ROLE_COMPANY
+}
+
--- src/main/java/org/miles/enumeration/UserAccountStatus.java
+++ src/main/java/org/miles/enumeration/UserAccountStatus.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.miles.enumeration;
 
-/**
- *
- * @author Ubaita
- */
 public enum UserAccountStatus {
-    
+    ACTIVE, INACTIVE, SUSPENDED
 }
