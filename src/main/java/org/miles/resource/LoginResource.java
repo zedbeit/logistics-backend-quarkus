26be3e7141a06667fcaf11305737d6ce71c85cd1
zedbeit <devbeita@gmail.com>
2021-09-21 18:13:27 +0100

    added openapi documentation of endpoints

--- src/main/java/org/miles/resource/LoginResource.java
+++ src/main/java/org/miles/resource/LoginResource.java
@@ -2,16 +2,19 @@ package org.miles.resource;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.miles.service.LoginService;
 import org.miles.service.dto.LoginDTO;
 
 @Path("auth")
+@Tag(ref = "Login")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class LoginResource {
@@ -20,6 +23,8 @@ public class LoginResource {
     LoginService loginService;
     
     @POST
+    @Operation(summary = "Login endpoint.")
+    @APIResponse(responseCode = "200", description = "returns a User Representation.")
     @Path("login")
     public Response login(LoginDTO loginDTO){
         return loginService.login(loginDTO.username,loginDTO.password);
--- src/main/java/org/miles/resource/UserAccountResource.java
+++ src/main/java/org/miles/resource/UserAccountResource.java
@@ -1,7 +1,6 @@
 package org.miles.resource;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,11 +8,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.miles.service.UserAccountService;
 import org.miles.service.dto.UserAccountDTO;
 import org.miles.service.dto.vm.UserAccountVM;
 
 @Path("account")
+@Tag(ref = "Account")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserAccountResource {
@@ -27,6 +31,8 @@ public class UserAccountResource {
     }    
     
     @POST
+    @Operation(summary = "Sign up endpoint for General user.")
+    @APIResponse(responseCode = "200", description = "returns a User Account DTO.")
     @Path("/generaluser-signup")
     public Response signUpForUser(UserAccountVM userAccountVM) {
         UserAccountDTO userAccount = userAccountService.createGeneralUser(userAccountVM);
@@ -34,6 +40,8 @@ public class UserAccountResource {
     }
     
     @POST
+    @Operation(summary = "Sign up endpoint for company.")
+    @APIResponse(responseCode = "200", description = "returns a User Account DTO.")
     @Path("/company-signup")
     public Response signUpForCompany(UserAccountVM userAccountVM) {
         UserAccountDTO userAccount = userAccountService.createCompany(userAccountVM);
